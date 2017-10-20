40a8052a31afa96296cf8c33cd777d78e4cae6d2
Mohd Huzaifa Faruqui <huzaifafaruqui@outlook.com>
2017-10-20 16:17:53 +0530

    Create ProducerConsumer.java

---
+++ Semaphores/ProducerConsumer.java
@@ -0,0 +1,84 @@
+
+package multithreading;
+
+import java.util.*;
+
+/**
+ *
+ * @author huzaifafaruqui
+ */
+class PC {
+
+    LinkedList<Integer> list = new LinkedList<>();
+    int bufferSize = 3;
+
+    public synchronized void produce(int item) throws InterruptedException {
+
+        if (list.size() == bufferSize) {
+            wait();
+        }
+
+        list.add(item);
+        System.out.println("added" + item);
+
+        notify();
+    }
+
+    public synchronized void consume() throws InterruptedException {
+
+        if (list.isEmpty()) {
+            wait();
+        }
+
+        System.out.println("removed" + list.peekLast());
+        list.removeLast();
+
+        notify();
+    }
+}
+
+public class ProducerConsumer {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        PC pc = new PC();
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+
+                while (i < 10) {
+                    try {
+                        pc.produce(i);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    ++i;
+                }
+            }
+        });
+
+        // Create consumer thread
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int j = 0;
+                while (j < 10) {
+                    try {
+                        pc.consume();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    ++j;
+                }
+            }
+        });
+
+        t1.start();
+
+        t2.start();
+    }
+
+}
